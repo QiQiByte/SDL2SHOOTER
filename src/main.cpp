54b60c2a6ded5b31c9a41ac95ee589d6fba6b316
isBaiqiqi <isbaiqiqi@gmail.com>
2025-08-22 17:52:21 +0800

    初始化项目结构，添加CMake配置和主程序文件，SDL2

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
